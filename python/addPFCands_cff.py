f71656a88bfeef13d51f9e544baf206033cab647
Benedikt Maier <bmaier@t3home000.mit.edu>
2020-09-16 11:11:06 -0400

    adding PF stuff

---
+++ python/addPFCands_cff.py
@@ -0,0 +1,33 @@
+import FWCore.ParameterSet.Config as cms
+from  PhysicsTools.NanoAOD.common_cff import *
+
+def addPFCands(process):
+    process.customizedPFCandsTask = cms.Task( )
+    process.schedule.associate(process.customizedPFCandsTask)
+    candInput = cms.InputTag("packedPFCandidates")
+
+    process.customConstituentsExtTable = cms.EDProducer("SimpleCandidateFlatTableProducer",
+                                                        src = candInput,
+                                                        cut = cms.string(""), #we should not filter after pruning
+                                                        name = cms.string("JetPFCands"),
+                                                        doc = cms.string("interesting particles from AK4 and AK8 jets"),
+                                                        singleton = cms.bool(False), # the number of entries is variable
+                                                        extension = cms.bool(False), # this is the extension table for the AK8 constituents
+                                                        variables = cms.PSet(CandVars,
+                                                            puppiWeight = Var("puppiWeight()", float, doc="Puppi weight",precision=10),
+                                                            puppiWeightNoLep = Var("puppiWeightNoLep()", float, doc="Puppi weight removing leptons",precision=10),
+                                                            vtxChi2 = Var("?hasTrackDetails()?vertexChi2():-1", float, doc="vertex chi2",precision=10),
+                                                            trkChi2 = Var("?hasTrackDetails()?pseudoTrack().normalizedChi2():-1", float, doc="normalized trk chi2", precision=10),
+                                                            dz = Var("?hasTrackDetails()?dz():-1", float, doc="pf dz", precision=10),
+                                                            dzErr = Var("?hasTrackDetails()?dzError():-1", float, doc="pf dz err", precision=10),
+                                                            d0 = Var("?hasTrackDetails()?dxy():-1", float, doc="pf d0", precision=10),
+                                                            d0Err = Var("?hasTrackDetails()?dxyError():-1", float, doc="pf d0 err", precision=10),
+                                                            pvAssocQuality = Var("pvAssociationQuality()", int, doc="primary vertex association quality"),
+                                                            lostInnerHits = Var("lostInnerHits()", int, doc="lost inner hits"),
+                                                            trkQuality = Var("?hasTrackDetails()?pseudoTrack().qualityMask():0", int, doc="track quality mask"),
+                                                        )
+                                                       )
+
+    process.customizedPFCandsTask.add(process.customConstituentsExtTable)
+            
+    return process
