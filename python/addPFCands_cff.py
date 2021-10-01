32705c256dbfcd98dadc6df2b9a4ec5cdbfae09f
Chad Freer <freerc@submit01.mit.edu>
2021-10-01 11:09:37 -0400

    adding track information 	modified:   python/addPFCands_cff.py

--- python/addPFCands_cff.py
+++ python/addPFCands_cff.py
@@ -32,6 +32,28 @@ def addPFCands(process):
                                                         )
                                             )
 
+    process.customIsolatedTracksTable = cms.EDProducer("SimpleCandidateFlatTableProducer",
+                                                        src = cms.InputTag("isolatedTracks"),
+                                                        cut = cms.string(""),
+                                                        name = cms.string("isolatedTracks"),
+                                                        doc = cms.string("isolated Tracks"),
+                                                        singleton = cms.bool(False),
+                                                        extension = cms.bool(False),
+                                                        variables = cms.PSet(P3Vars,
+                                                            dz = Var("dz",float,doc="dz (with sign) wrt first PV, in cm",precision=10),
+                                                            dxy = Var("dxy",float,doc="dxy (with sign) wrt first PV, in cm",precision=10),
+                                                            pfRelIso03_chg = Var("pfIsolationDR03().chargedHadronIso/pt",float,doc="PF relative isolation dR=0.3, charged component",precision=10),
+                                                            pfRelIso03_all = Var("(pfIsolationDR03().chargedHadronIso + max(pfIsolationDR03().neutralHadronIso + pfIsolationDR03().photonIso - pfIsolationDR03().puChargedHadronIso/2,0.0))/pt",float,doc="PF relative isolation dR=0.3, total (deltaBeta corrections)",precision=10),
+                                                            isPFcand = Var("packedCandRef().isNonnull()",bool,doc="if isolated track is a PF candidate"),
+                                                            fromPV = Var("fromPV", int, doc="isolated track comes from PV"),
+                                                            pdgId = Var("pdgId",int,doc="PDG id of PF cand"),
+                                                            isHighPurityTrack = Var("isHighPurityTrack",bool,doc="track is high purity"),
+                                                            charge = Var("charge", int, doc="electric charge"),
+                                                        )
+                                            )
+
+
     process.customizedPFCandsTask.add(process.customConstituentsExtTable)
+    process.customizedPFCandsTask.add(process.customIsolatedTracksTable)
 
     return process
