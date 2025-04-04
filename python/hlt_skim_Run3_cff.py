3cdd6cbf76a426535550e62ade22f18291f1a9db
Christos Papageorgakis <chrispap95@gmail.com>
2025-04-04 10:47:39 -0500

    Adding files for Run3

--- python/hlt_skim_Run3_cff.py
+++ python/hlt_skim_Run3_cff.py
@@ -10,7 +10,6 @@ hltHighLevel = cms.EDFilter(
         "HLT_TripleMu_10_5_5_DZ_v*",
         "HLT_TripleMu_12_10_5_v*",
         "HLT_TripleMu_5_3_3_Mass3p8_DZ_v*",
-        "HLT_TripleMu_5_3_3_Mass3p8to60_DZ_v*",
     ),
     eventSetupPathsKey=cms.string(""),
     andOr=cms.bool(True),  # True (OR) accept if ANY is true
--- test/datasets/converter.py
+++ test/datasets/converter.py
@@ -1,4 +1,23 @@
-# This script can be used to convert dataset names between different years.
+"""
+This script can be used to convert dataset names between different years.
+It takes a JSON file as input, which contains the desired primary datasets,
+and a secondary dataset name which is the target MC campaign. It then queries
+the DAS (Data Aggregation Service) to get the list of datasets for the
+specified primary and secondary datasets. The output is a JSON file
+containing the list of datasets.
+
+Usage:
+    python converter.py --input <input_json_file> --secondary <secondary_dataset_name>
+Example (to get 2022EE from 2022):
+    python converter.py --input 2022/full_mc_2022.json --secondary Run3Summer22EEMiniAODv4-130X_mcRun3_2022_realistic_postEE_v6
+
+Secondary dataset names:
+    - Run3Summer22EEMiniAODv4-130X_mcRun3_2022_realistic_postEE_v
+    - Run3Summer22MiniAODv4-130X_mcRun3_2022_realistic_v
+    - Run3Summer23BPixMiniAODv4-130X_mcRun3_2023_realistic_postBPix_v
+    - Run3Summer23MiniAODv4-130X_mcRun3_2023_realistic_v
+"""
+
 import argparse
 import json
 import shlex
@@ -13,14 +32,19 @@ def get_args():
     parser.add_argument(
         "--input",
         type=str,
-        help="Input JSON file. Make sure the desired primary datasets are inside.",
-        default="2016/full_mc_2016.json",
+        help="Input JSON file. Make sure the desired primary datasets are inside. E.g., 2022/full_mc_2022.json",
+        required=True,
     )
     parser.add_argument(
         "--secondary",
         type=str,
-        help="Secondary dataset name. Example: RunIISummer20UL16MiniAODv2-106X_mcRun2_asymptotic_v17",
-        default="RunIISummer20UL16MiniAODv2-106X_mcRun2_asymptotic_v17",
+        help="Secondary dataset name. Example: Run3Summer22EEMiniAODv4-130X_mcRun3_2022_realistic_postEE_v6",
+        required=True,
+    )
+    parser.add_argument(
+        "--output",
+        type=str,
+        help="Output JSON file. Default is <input_json_file>_correct.json",
     )
     return parser.parse_args()
 
@@ -30,6 +54,8 @@ if __name__ == "__main__":
     with open(args.input) as json_file:
         datasets = json.load(json_file)
 
+    # Get the primary dataset names from the input JSON file
+    # and remove duplicates
     primaries = list(set([p.split("/")[1] for p in datasets]))
 
     list_of_datasets = []
@@ -43,5 +69,8 @@ if __name__ == "__main__":
         out, err = p.communicate()
         list_of_datasets += [f for f in out.decode("utf-8").split("\n") if f]
 
-    with open(args.input.replace(".json", "_correct.json"), "w") as json_file:
-        json.dump(list_of_datasets, json_file, indent=4)
+    output_file = args.output
+    if output_file is None:
+        output_file = args.input.replace(".json", "_correct.json")
+    with open(output_file, "w") as json_file:
+        json.dump(sorted(list_of_datasets), json_file, indent=4)
